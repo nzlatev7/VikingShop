dc1e2ff79e2b9c65c97fc299607d5c058876e6d9
PC <PC@ZLATI>
2022-06-08 10:47:54 +0300

    x

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VikingShop.Models.Request;
 
 namespace VikingShop.Controllers
 {
@@ -23,8 +24,37 @@ namespace VikingShop.Controllers
         }
 
         [HttpPost]
+        public Response Register(RegisterRequest request) 
+        {
+            if(request.UserName.Length > 40 || request.UserName.Length < 3)
+            {
+                return new Response(false, "UserNameLength");
+            }
+            if (dbContext.Users.Any(x => x.UserName == request.UserName))
+            {
+                return new Response(false, "UserNameDublicate");
+            }
 
-    
+            if (request.Password.Length < 8 || request.Password.Length > 40)
+            {
+                return new Response(false, "PasswordLength");
+            }
+            if (dbContext.Users.Any(x => x.Email == request.Email))
+            {
+                return new Response(false, "EmailDublicate");
+            }
+            dbContext.Users.Add(new Models.User()
+            {
+                UserName = request.UserName,
+                Password = request.Password,
+                Email = request.Email,
+                IsEmailConfirmed = false,
+                
+            });
 
+            dbContext.SaveChanges();
+            return new Response(true, "");
+
+        }
     }
 }
