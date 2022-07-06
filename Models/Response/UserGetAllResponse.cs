6e77217635c1d853fd6243390907aeca617f9c88
Nikolay <nikolay.zlatev2005@gmail.com>
2022-07-06 22:10:58 +0300

    add Response model(UserGetAllResponse)

---
+++ Models/Response/UserGetAllResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VikingShop.Models.Response
+{
+    public class UserGetAllResponse
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public bool? IsEmailConfirmed { get; set; }
+    }
+}
